ba9b3fa1ba07062687bf1daf7302a7328d039d5d
Alexander Hogle <alexanderhogle@gmail.com>
2023-08-28 16:10:32 -0500

    builds and calls useFetch function to fetch data from api

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,8 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import { RAPID_API_KEY } from '@env';
 
-const rapidApiKey = RAPID_API_KEY;
+const rapidApiKey = process.env.REACT_APP_NOT_SECRET_CODE;
 
 const useFetch = (endpoint, query) => {
   const [data, setData] = useState([]);
@@ -18,4 +17,33 @@ const useFetch = (endpoint, query) => {
     },
     params: { ...query }
   };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await axios.request(options);
+
+      setData(response.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      alert('There is an error');
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refetch = () => {
+    setIsLoading(true);
+    fetchData();
+  };
+
+  return { data, isLoading, error, refetch };
 };
+
+export default useFetch;
