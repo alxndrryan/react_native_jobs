57f335b9952eb1dc2e7a6f053ee5dc672ac08b46
Alexander Hogle <alexanderhogle@gmail.com>
2023-09-01 19:05:09 -0500

    Layout for company and jobtabs components. props set up for company component

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -29,6 +29,10 @@ const JobDetails = () => {
     job_id: params.id
   });
 
+  const [refreshing, setRefreshing] = useState(false);
+
+  const onRefresh = () => {};
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
       <Stack.Screen
@@ -48,7 +52,34 @@ const JobDetails = () => {
           ),
           headerTitle: ''
         }}
-      ></Stack.Screen>
+      />
+
+      <>
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          }
+        >
+          {isLoading ? (
+            <ActivityIndicator size="large" color={COLORS.primary} />
+          ) : error ? (
+            <Text>Something went wrong</Text>
+          ) : data.length === 0 ? (
+            <Text>No data</Text>
+          ) : (
+            <View style={{ padding: SIZES.medium, paddingBottom: 100 }}>
+              <Company
+                companyLogo={data[0].employer_logo}
+                jobTitle={data[0].job_title}
+                companyName={data[0].employer_name}
+                Location={data[0].job_country}
+              />
+              <JobTabs />
+            </View>
+          )}
+        </ScrollView>
+      </>
     </SafeAreaView>
   );
 };
